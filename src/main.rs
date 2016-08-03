af9c11d51f308c29610236755e78b76f5e1f7695
hannelita <hannelita@gmail.com>
2016-08-03 14:19:47 -0300

    improvement 1

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,24 @@
 extern crate hyper;
 
 
-use std::env;
-use std::io;
-use std::sync::mpsc;
-use std::time::Duration;
+use hyper::Client;
+use hyper::header::Connection;
+use std::io::Read;
 
-use hyper::client::{Client, Request, Response};
 
+// fn main() {
+//     let client = Client::new();
+
+//     let res = client.get("http://www.amazon.com.br/registry/wishlist/3DA4I0ZLH8ADW/ref=cm_sw_r_tw_ws_9hJzwb06V29HS").send().unwrap();
+
+//     println!("Response: {}", res.body);
+// }
 
 fn main() {
     let client = Client::new();
-
-    let res = client.get("http://www.amazon.com.br/registry/wishlist/3DA4I0ZLH8ADW/ref=cm_sw_r_tw_ws_9hJzwb06V29HS").send().unwrap();
-
-    println!("Response: {}", res.body);
+    let mut response = client.get("http://www.amazon.com.br/registry/wishlist/3DA4I0ZLH8ADW/ref=cm_sw_r_tw_ws_9hJzwb06V29HS").
+        header(Connection::close()).send().unwrap();
+    let mut body = String::new();
+    response.read_to_string(&mut body).unwrap();
+    println!("Body:   {}", body);
 }
