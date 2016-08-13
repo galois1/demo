85aeda53e18117c6879dc2e3c080f5da5c9c13f6
Zaki Manian <zaki@manian.org>
2016-08-13 12:01:30 -0700

    Revert back to the broken version but not that it is expected to fail

--- step00/src/main.rs
+++ step00/src/main.rs
@@ -7,15 +7,13 @@ use std::time::Duration;
 use std::io::Read;
 use hyper::client::{Client, Request, Response};
 
+//This is expected to fail to compile.
 
 fn main() {
-    let client = Client::new();
+        let client = Client::new();
 
-    let mut res = client.get("http://www.amazon.com.\
-              br/registry/wishlist/3DA4I0ZLH8ADW/ref=cm_sw_r_tw_ws_9hJzwb06V29HS")
-        .send()
-        .unwrap();
-    let mut body = String::new();
-    res.read_to_string(&mut body);
-    println!("Response: {}", body);
+        let res = client.get("http://www.amazon.com.br/registry/wishlist/3DA4I0ZLH8ADW/ref=cm_sw_r_tw_ws_9hJzwb06V29HS").send().unwrap();
+
+        println!("Response: {}", res.body);
+    }
 }
