e3708932e42d3aff5c63be068e314a489c9898e1
Brian Anderson <banderson@mozilla.com>
2016-08-13 11:38:43 -0700

    Restructure and fix openssl fails

---
+++ step01/src/main.rs
@@ -0,0 +1,24 @@
+extern crate hyper;
+
+
+use hyper::Client;
+use hyper::header::Connection;
+use std::io::Read;
+
+
+// fn main() {
+//     let client = Client::new();
+
+//     let res = client.get("http://www.amazon.com.br/registry/wishlist/3DA4I0ZLH8ADW/ref=cm_sw_r_tw_ws_9hJzwb06V29HS").send().unwrap();
+
+//     println!("Response: {}", res.body);
+// }
+
+fn main() {
+    let client = Client::new();
+    let mut response = client.get("http://www.amazon.com.br/registry/wishlist/3DA4I0ZLH8ADW/ref=cm_sw_r_tw_ws_9hJzwb06V29HS").
+        header(Connection::close()).send().unwrap();
+    let mut body = String::new();
+    response.read_to_string(&mut body).unwrap();
+    println!("Body:   {}", body);
+}
