137976de8959f2eb39a6c1fa25599f226a97f805
Emilio Cobos Álvarez <ecoal95@gmail.com>
2016-08-13 14:39:58 -0700

    Iterate over books to print them.

--- step999/scraper/src/main.rs
+++ step999/scraper/src/main.rs
@@ -13,22 +13,25 @@ use std::io::Read;
 fn main() {
     let client = Client::new();
     let mut response = client.get("https://brson.github.io/demo/wishlist.html")
-                             .header(Connection::close())
                              .send()
                              .unwrap();
     let mut body = String::new();
     response.read_to_string(&mut body).unwrap();
-    let items: Vec<(String, String)> = Document::from(body.as_str())
-                   .find(Class("a-row"))
-                   .iter()
-                   .flat_map(|node| {
-                      let title = node.find(Name("h5")).first();
-                      let price = node.find(Class("a-color-price")).first();
-                      match (title, price) {
-                          (Some(title), Some(price)) => Some((title.text().trim().into(), price.text().trim().into())),
-                          _ => None
-                      }
-                   })
-                   .collect();
-    println!("{:?}", items);
+    let items: Vec<(String, String)> =
+        Document::from(body.as_str())
+                  .find(Class("a-row"))
+                  .iter()
+                  .flat_map(|node| {
+                     let title = node.find(Name("h5")).first();
+                     let price = node.find(Class("a-color-price")).first();
+                     match (title, price) {
+                         (Some(title), Some(price)) => Some((title.text().trim().into(), price.text().trim().into())),
+                         _ => None
+                     }
+                  })
+                  .collect();
+
+    for (title, price) in items {
+        println!(" * Book \"{}\", with price {}", title, price);
+    }
 }
